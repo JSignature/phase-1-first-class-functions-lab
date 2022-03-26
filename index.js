50cf118544d2e8c0f2aceee776bdfdc2aba80431
Jimmy Andreas <jimmysignature@gmail.com>
2022-03-25 21:30:37 -0400

    stuck on third function

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (params) {
+    const drivers = []
+    for (let index = 0; index < 2; index++) {
+      drivers.push(params[index])
+        
+    }
+    return drivers
+}
+
+const returnLastTwoDrivers = function (params) {
+    const drivers = []
+    const paramsCut = params.slice(-2)
+   for (let index = 0; index < 2; index++) {
+      drivers.push(paramsCut[index])
+       
+   }
+   return drivers
+    
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(int) {
+    return function secondFunction (fare) { 
+        fare * int
+
+    }
+
+}
+
